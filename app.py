445a3285313b9da133cff57ebd604a3464934c10
sakshi <ssakshi182005@gmail.com>
2026-06-25 20:57:55 +0530

    Added lists and loops

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 industry = input("Enter an industry: ")
 
-print("\nStartup Idea:")
-print("AI-powered assistant for", industry)git add .
+ideas = [
+    "AI Assistant",
+    "AI Analytics Platform",
+    "AI Marketplace",
+    "AI Automation Tool",
+    "AI Recommendation System"
+]
+
+print("\nStartup Ideas:")
+
+for idea in ideas:
+    print("-", idea, "for", industry)
